fc6f52bc8b630c50d11870c0ff341904a88668e8
Alexandr Irza <alexandr.irza@gmail.com>
2020-07-28 22:49:53 +0300

    Finished 1.1-1.3 tasks Started working with 2.1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,36 +5,120 @@ namespace ConsoleApp
 {
     class Program
     {
+        static void Output(string msg, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void OutputError(string msg)
+        {
+            Output(msg, ConsoleColor.Red);
+        }
+
+        static string ReverseString(string input)
+        {
+            var res = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                res += input[i];
+            }
+            return res;
+        }
+
         static void Main(string[] args)
         {
+            var fileName = "";
             var param = "";
             if (args.Length == 0)
             {
+                Output("Enter file name:", ConsoleColor.Yellow);
+                fileName = Console.ReadLine();
+                Output("Enter word to replace:", ConsoleColor.Yellow);
                 param = Console.ReadLine();
             }
             else
             {
-                param = args[0];
+                fileName = args[0];
+                param = args[1];
             }
 
-            param = AppDomain.CurrentDomain.BaseDirectory + param;
+            fileName = AppDomain.CurrentDomain.BaseDirectory + fileName;
 
-            if (File.Exists(param))
+            if (File.Exists(fileName))
             {
-                var text = File.ReadAllText(param);
-                char[] delimiterChars = { ' ', ',', '.', ':', '\r', '\n', '?', '!', '(', ')' };
+                var text = File.ReadAllText(fileName);
 
-                var words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
-
-                for (var i = 0; i < words.Length; i++)
+                #region 1 task replace word
+                if (param.Length > 0 && text.Contains(param))
                 {
-                    if (i % 10 == 0)
-                    Console.Write(words[i] + ", ");
+                    //Store original file
+                    File.Copy(fileName, fileName + ".origin", true);
+                    text = text.Replace(param, "", true, null);
+                    File.WriteAllText(fileName, text);
+                    Output(string.Format("Text without the word \"{0}\"", param), ConsoleColor.Green);
+                    Output(text);
                 }
+                else
+                {
+                    OutputError(string.Format("Text does not contain the word \"{0}\"", param));
+                    Output(text);
+                }
+                #endregion
+                #region 2 task count words and show every 10
+                text = File.ReadAllText(fileName);
+                var wordDelimiters = new string[] { " ", ",", ".", ":", "\r\n", Environment.NewLine, "?", "!", "(", ")", "\"" };
+
+                var words = text.Split(wordDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                Output("Words count:", ConsoleColor.Green);
+                Output(words.Length.ToString());
+
+                var res = "";
+                
+                for (var i = 0; i < words.Length; i = i + 10)
+                {
+                    res += (res.Length != 0 ? ", " : "") + words[i];
+                }
+
+                Output("Every 10th word separated by comma:", ConsoleColor.Green);
+                Output(res);
+                #endregion
+
+                #region 3 task show third sentence
+
+                text = File.ReadAllText(fileName);
+                var sentenceDelimiters = new string[] { ".", "!", "?" };
+
+                var sentences = text.Split(sentenceDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                if (sentences.Length > 3)
+                {
+                    var sentence = sentences[2];
+                    Output("Third sentence:", ConsoleColor.Green);
+                    Output(sentence);
+
+                    words = sentence.Split(" ");
+
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        words[i] = ReverseString(words[i]);
+                    }
+
+                    Output("Third sentence with reversed words:", ConsoleColor.Green);
+
+                    Output(string.Join(" ", words));
+                }
+                else
+                    OutputError("Sentence #3 does not exist.");
+
+                #endregion
+
             }
             else
             {
-                Console.WriteLine("File {0} not found", param);
+                OutputError(string.Format("File {0} not found", fileName));
             }
 
         }
