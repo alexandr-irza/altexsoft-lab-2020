702c9914acc5bf6fc11ca972c8069cb041e869dd
Alexandr Irza <alexandr.irza@gmail.com>
2020-07-28 21:22:17 +0300

    Init project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var param = "";
+            if (args.Length == 0)
+            {
+                param = Console.ReadLine();
+            }
+            else
+            {
+                param = args[0];
+            }
+
+            param = AppDomain.CurrentDomain.BaseDirectory + param;
+
+            if (File.Exists(param))
+            {
+                var text = File.ReadAllText(param);
+                char[] delimiterChars = { ' ', ',', '.', ':', '\r', '\n', '?', '!', '(', ')' };
+
+                var words = text.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+                for (var i = 0; i < words.Length; i++)
+                {
+                    if (i % 10 == 0)
+                    Console.Write(words[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("File {0} not found", param);
+            }
+
+        }
+    }
+}
