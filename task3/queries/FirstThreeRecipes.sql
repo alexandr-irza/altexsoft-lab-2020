def6d44439c234f3a123944908e7959446d13a60
Alexandr Irza <alexandr.irza@gmail.com>
2020-09-17 14:39:45 +0300

    Changed script to do not use 'LIKE'

--- task3/queries/FirstThreeRecipes.sql
+++ task3/queries/FirstThreeRecipes.sql
@@ -2,23 +2,26 @@
 --чтобы общее количество записей было 3. Например, у Вас есть в категории с айди=3 один рецепт, 
 --а также еще 3 подкатегории, и в каждой по 2 рецепта. В результате должно быть: 1 запись из 3 категории и еще 2 записи из подкатегории
 
+declare @CategoryId int;
+set @CategoryId = 3;
+
 with Categories_CTE(CategoryId, CategoryParentId, CategoryName, CategoryPath)
 AS
 (
-	select c.Id, c.ParentId, c.Name, cast(c.Id as nvarchar(max)) + '\' as CategoryPath
+	select c.Id, c.ParentId, c.Name, cast(c.Name as nvarchar(max)) + '\' as CategoryPath
 	from Categories c
-	where c.ParentId is null
+	where c.Id = @CategoryId
 	union all
-	select c.Id, c.ParentId, c.Name, CategoryPath + cast(c.Id as nvarchar(max)) + '\'
+	select c.Id, c.ParentId, c.Name, CategoryPath + cast(c.Name as nvarchar(max)) + '\'
 	from Categories c
 	inner join Categories_CTE cc on cc.CategoryId = c.ParentId
+	where cc.CategoryId = @CategoryId or cc.CategoryParentId = @CategoryId
 )
 
 select x.Id as RecipeId, x.Name as RecipeName, ri.IngredientId, i.Name as IngredientName, x.CategoryId, x.CategoryPath
 from ( select top 3 r.Id, r.Name, c.CategoryId, c.CategoryPath
 	from Recipes r
 	inner join Categories_CTE c on c.CategoryId = r.CategoryId
-	where c.CategoryPath like '3\%'
 	order by c.CategoryPath) x
 left join RecipeIngredients ri on ri.RecipeId = x.Id
 left join Ingredients i on ri.IngredientId = i.Id
