5dfe88abe723ac8f23d98eb94009331613daa6c3
Alexandr Irza <alexandr.irza@gmail.com>
2020-09-22 09:24:33 +0300

    Fixed hardcoded connection string

--- RecipeBook2/RecipeBook2.Infrastructure/Extensions/RecipeBookContextFactory.cs
+++ RecipeBook2/RecipeBook2.Infrastructure/Extensions/RecipeBookContextFactory.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using RecipeBook2.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace RecipeBook2.Infrastructure.Extensions
 {
@@ -11,8 +10,15 @@ namespace RecipeBook2.Infrastructure.Extensions
     {
         public RecipeBookContext CreateDbContext(string[] args)
         {
+            // Build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../RecipeBook2.Cmd"))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+                
             var optionsBuilder = new DbContextOptionsBuilder<RecipeBookContext>();
-            optionsBuilder.UseSqlServer("Server=localhost;Database=RecipeBook;Trusted_Connection=True;");
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
 
             return new RecipeBookContext(optionsBuilder.Options);
         }
