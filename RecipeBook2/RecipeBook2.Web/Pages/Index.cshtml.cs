13eb61952dd380dd98d7581fd17294e42739de32
Alexandr Irza <alexandr.irza@gmail.com>
2020-11-11 21:16:40 +0200

    Removed logger

--- RecipeBook2/RecipeBook2.Web/Pages/Index.cshtml.cs
+++ RecipeBook2/RecipeBook2.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RecipeBook2.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel()
         {
-            _logger = logger;
         }
 
         public void OnGet()
